247308cf064b4786b9f2139b3c963c36daa8b431
READYonHub <readyemail3@gmail.com>
2026-04-08 14:05:36 +0200

    17. neon db connection + middlewares auth

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
+
+app.use(requireAuth())
 
 app.get("/", (req, res) => {
   res.send("Server is LIVE!");
